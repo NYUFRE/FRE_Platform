9a45668d287a645967b2fcaebfbf09a702e222ef
KodaSong <kkoda626@gmail.com>
2021-02-24 17:50:46 -0500

    Rewrite GA part
    Write GA in a more pythonic way.

--- system/ai_modeling/ga_portfolio_probation_test.py
+++ system/ai_modeling/ga_portfolio_probation_test.py
@@ -8,6 +8,8 @@ sys.path.append('../')
 import datetime as dt
 import holidays
 from system.ai_modeling.ga_portfolio import GAPortfolio, Stock, Trade
+from system.ai_modeling.ga_portfolio_select import extract_spy, extract_us10y
+from typing import Tuple
 
 fund = 1000000
 
@@ -24,102 +26,67 @@ def previous_working_day(check_day_, holidays=holidays.US()):
         return previous_working_day(most_recent, holidays)
 
 probation_testing_start_date = (dt.date(2020, 6, 30) + dt.timedelta(days=1)).strftime('%Y-%m-%d')
-# probation_testing_end_date = previous_working_day(dt.datetime.today()).strftime('%Y-%m-%d')
-probation_testing_end_date = previous_working_day(dt.date(2020, 8, 1)).strftime('%Y-%m-%d')
+probation_testing_end_date = (dt.date(2021, 2, 19)).strftime('%Y-%m-%d')
+# probation_testing_end_date = previous_working_day(dt.date(2020, 8, 1)).strftime('%Y-%m-%d')
 
-def ga_probation_test(database):
+def ga_probation_test(database) -> Tuple[GAPortfolio, float, int]:
+    """
+    description:
+        Do probation test from 2020-07-01 till today
+    ---------------------------
+    return:
+        best_portfolio, spy_profit_loss, fund: Tuple[GAPortfolio, float, int]
+    """
+    # Extract best portfolio's data from table best_portfolio
+    best_portfolio_select = "SELECT symbol, sector, category_pct from best_portfolio;"
+    best_portfolio_df = database.execute_sql_statement(best_portfolio_select)
+    best_portfolio_symbols = list(best_portfolio_df['symbol'])
+
+    # Extract stock price data from table stocks
+    stock_select = "SELECT stocks.symbol, stocks.date, stocks.open, stocks.close, sp500.name, sp500_sectors.category_pct AS weight\
+                    FROM stocks, sp500, sp500_sectors\
+                    WHERE stocks.symbol = sp500.symbol\
+                    AND sp500.sector = sp500_sectors.sector\
+                    AND strftime(\'%Y-%m-%d\', date) BETWEEN \"" + probation_testing_start_date + "\" AND \"" + \
+                    probation_testing_end_date + "\" AND open > 0 AND close > 0;"
+    price_df = database.execute_sql_statement(stock_select)
+
     best_portfolio = GAPortfolio()
+    best_portfolio.populate_portfolio_by_symbols(best_portfolio_symbols, price_df)  # Calculate returns
 
-    best_portfolio_select = "SELECT symbol, name as symbol_name, sector, category_pct from best_portfolio;"
-    print(best_portfolio_select)
-    stock_df = database.execute_sql_statement(best_portfolio_select)
-    for index, stock_row in stock_df.iterrows():
-        stock = Stock()
-        stock.symbol = stock_row['symbol']
-        stock.name = stock_row['symbol_name']
-        stock.sector = stock_row['sector']
-        stock.category_pct = stock_row['category_pct']
-        stock_select = "SELECT * FROM stocks WHERE strftime(\'%Y-%m-%d\', date) " \
-                       "BETWEEN \"" + probation_testing_start_date + "\" AND \"" + probation_testing_end_date + \
-                       "\" AND open > 0 AND close > 0 AND symbol = \'" + stock.symbol + "\';"
-        print(stock_select)
-        price_df = database.execute_sql_statement(stock_select)
-        if price_df.empty:
-            exit("probation testing price_df is empty")
-        for index, trade_row in price_df.iterrows():
-            trade = Trade()
-            trade.date = trade_row['date']
-            trade.open = trade_row['open']
-            trade.high = trade_row['high']
-            trade.low = trade_row['low']
-            trade.close = trade_row['close']
-            trade.adjusted_close = trade_row['adjusted_close']
-            trade.volume = trade_row['volume']
-            stock.add_trade(trade)
-
-        stock.calculate_daily_return()
-        best_portfolio.stocks.append(stock)
-
-    best_portfolio.calculate_portfolio_daily_return()
-
+    # Update table best_portfolio
     conn = database.engine.connect()
-
-    for i in range(len(best_portfolio.stocks)):
+    for symbol in best_portfolio_symbols:
         open_date = probation_testing_start_date
         close_date = probation_testing_end_date
-        open_price = best_portfolio.stocks[i].trades[probation_testing_start_date].adjusted_close
-        close_price = best_portfolio.stocks[i].trades[probation_testing_end_date].adjusted_close
-        shares = int(fund * best_portfolio.stocks[i].category_pct/open_price)
+        # open_price = best_portfolio.stocks[i].trades[probation_testing_start_date].adjusted_close
+        # close_price = best_portfolio.stocks[i].trades[probation_testing_end_date].adjusted_close
+        df_slice = best_portfolio.price_df[best_portfolio.price_df.symbol == symbol]
+        open_price = df_slice['open'].iloc[0]
+        close_price = df_slice['close'].iloc[-1]
+        shares = int(fund * best_portfolio.price_df[best_portfolio.price_df.symbol == symbol]['weight'].iloc[0] / open_price)
         profit_loss = (close_price - open_price) * shares
-        best_portfolio.stocks[i].probation_test_trade.open_date = open_date
-        best_portfolio.stocks[i].probation_test_trade.close_date = close_date
-        best_portfolio.stocks[i].probation_test_trade.open_price = open_price
-        best_portfolio.stocks[i].probation_test_trade.close_price = close_price
-        best_portfolio.stocks[i].probation_test_trade.shares = shares
-        best_portfolio.stocks[i].probation_test_trade.profit_loss = profit_loss
+        best_portfolio.profit_loss += profit_loss
 
         update_stmt = "UPDATE best_portfolio SET open_date = \"" + str(open_date) + "\", open_price = " + str(open_price) + ", close_date = \"" + \
                       str(close_date) + "\", close_price = " + str(close_price) + ", shares = " + str(shares) + ", profit_loss = " + str(round(profit_loss, 2)) + \
-                      " WHERE symbol = \"" + best_portfolio.stocks[i].symbol + "\";"
+                      " WHERE symbol = \"" + symbol + "\";"
         print(update_stmt)
         conn.execute(update_stmt)
 
-    best_portfolio.calculate_profit_loss()
-
-
-    spy = Stock()
-    spy.symbol = 'SPY'
-
     # make SPY captical in spy table
-    spy_select = "SELECT * FROM spy WHERE strftime(\'%Y-%m-%d\', date) " \
+    spy_select = "SELECT date, open, close FROM spy WHERE strftime(\'%Y-%m-%d\', date) " \
                 "BETWEEN \"" + probation_testing_start_date + "\" AND \"" + probation_testing_end_date + \
                 "\" AND open > 0 AND close > 0 AND symbol = 'spy';"
-    print(spy_select)
-    price_df = database.execute_sql_statement(spy_select)
+    spy_df = database.execute_sql_statement(spy_select)
 
-    for index, row in price_df.iterrows():
-        trade = Trade()
-        trade.date = row['date']
-        trade.open = row['open']
-        trade.high = row['high']
-        trade.low = row['low']
-        trade.close = row['close']
-        trade.adjusted_close = row['adjusted_close']
-        trade.volume = row['volume']
-        spy.add_trade(trade)
-
-    spy.calculate_daily_return()
-
-    spy.probation_test_trade.open_date = probation_testing_start_date
-    spy.probation_test_trade.close_date = probation_testing_end_date
-    spy.probation_test_trade.open_price = spy.trades[probation_testing_start_date].open
-    spy.probation_test_trade.close_price = spy.trades[probation_testing_end_date].close
-    spy.probation_test_trade.shares = int(fund / spy.probation_test_trade.open_price)
-    spy.probation_test_trade.profit_loss = (spy.probation_test_trade.close_price - spy.probation_test_trade.open_price) * \
-                                           spy.probation_test_trade.shares
+    spy_open_price = spy_df.open.iloc[0]
+    spy_close_price = spy_df.close.iloc[-1]
+    spy_shares = int(fund / spy_open_price)
+    spy_profit_loss = (spy_close_price - spy_open_price) * spy_shares
 
     print("Probabtion Test:")
     print("best portfolio return: %4.2f%%" % (float(best_portfolio.profit_loss / fund) * 100))
-    print("spy return: %4.2f%%" % (float(spy.probation_test_trade.profit_loss / fund) * 100))
+    print("spy return: %4.2f%%" % (float(spy_profit_loss / fund) * 100))
 
-    return best_portfolio, spy, fund
+    return best_portfolio, spy_profit_loss, fund
