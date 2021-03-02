0938fceda163c52b9273a82c1fbb293c4375f344
ZichengHe <hehans1995@outlook.com>
2021-03-02 16:33:36 -0500

    Support Partial Fill Now

--- app.py
+++ app.py
@@ -253,7 +253,10 @@ def buy():
         if not best_ask > 0 or not quote["askSize"] > 0:
             flash('Order Rejected! No Selling Orders in the Market Now. Please Try Later.', 'error')
             return render_template("buy.html")
-
+        # Partial Fill
+        if quote["askSize"] < shares:
+            shares = quote["askSize"]
+            flash(f'Only {shares} share(s) of {symbol} available to buy from the market.', 'Notice')
         price = 0.0
         input_price = request.form.get('price')
         # When no input price -> Market order
@@ -316,6 +319,20 @@ def sell():
         if not shares > 0:
             flash('ERROR! Shares must be positive.', 'error')
             return render_template("sell.html")
+        # Get position info
+        portfolio = database.get_portfolio(session['user_id'], symbol)
+        existing_shares = 0
+        if len(portfolio['symbol']) == 1 and portfolio['symbol'][0] == symbol:
+            existing_shares = portfolio['shares'][0]
+        else:
+            flash('ERROR! Stock not in your portfolio.', 'error')
+            return render_template("sell.html")
+
+        cash = portfolio['cash']
+
+        if shares > existing_shares:
+            flash('ERROR! No enough shares to sell.', 'error')
+            return render_template("sell.html")
 
         # Get quote bid price -> potential sell price
         quote, error = iex_market_data.get_quote(symbol)
@@ -327,7 +344,10 @@ def sell():
         if not best_bid > 0 or not quote["bidSize"] > 0:
             flash('Order Rejected! No Buying Orders in the Market Now. Please Try Later.', 'error')
             return render_template("sell.html")
-
+        # Partial Fill
+        if quote["bidSize"] < shares:
+            shares = quote["bidSize"]
+            flash(f'Only {shares} share(s) of {symbol} available to sell to the market.', 'Notice')
         price = 0.0
         input_price = request.form.get('price')
         # Market order, Use the best bid price as selling price
@@ -349,20 +369,6 @@ def sell():
                       f'Now you sold {shares} share(s) of {symbol} at {usd(best_bid)}.', 'Notice')
                 price = best_bid
         uid = session['user_id']
-        # Get position info
-        portfolio = database.get_portfolio(session['user_id'], symbol)
-        existing_shares = 0
-        if len(portfolio['symbol']) == 1 and portfolio['symbol'][0] == symbol:
-            existing_shares = portfolio['shares'][0]
-        else:
-            flash('ERROR! Stock not in your portfolio.', 'error')
-            return render_template("sell.html")
-
-        cash = portfolio['cash']
-
-        if shares > existing_shares:
-            flash('ERROR! No enough shares to sell.', 'error')
-            return render_template("sell.html")
 
         updated_shares = existing_shares - shares
 
