3b032a0bd661f94fb5d9873f95e14f956dee8148
Albert Lee <47620142+albertlee0622@users.noreply.github.com>
2021-03-14 19:22:24 -0400

    Albert (#59)
    * Removed credentials (reload)
    
    Issue #29
    
    * Updated on comments by Louis
    
    #29
    
    * import create_engine
    
    #57
    
    * update server.py
    
    #58
    
    * typo fixed
    
    #58
    
    * Update trading logic
    
    #48 #66
    
    * delete a obsolete print()
    
    #48
    
    * updated on comments by Louis
    
    #48
    
    * sort PnL table by symbol
    
    #48

--- app.py
+++ app.py
@@ -815,7 +815,8 @@ def sim_server_down():
             client_config.client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
             status = client_config.client_socket.connect_ex(client_config.ADDR)
             if status != 0:
-                return error_page("Fail in connecting to server")
+                flash("Failure in server: please restart the program")
+                return render_template("sim_server_down.html")
 
             client_config.receiver_stop = False
             client_config.server_tombstone = True
@@ -860,8 +861,8 @@ def sim_auto_trading():
             client_config.client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
             status = client_config.client_socket.connect_ex(client_config.ADDR)
             if status != 0:
-                return error_page("Fail in connecting to server")
-
+                flash("Failure in server: please restart the program")
+                return render_template("error_auto_trading.html")
             client_config.client_up = True
             client_config.orders = []
             client_packet = Packet()
@@ -894,10 +895,15 @@ def sim_auto_trading():
             client_config.trade_complete = False
             client_config.client_socket.close()
 
-        return render_template("sim_auto_trading.html", trading_results=client_config.orders)
+        return render_template("sim_auto_trading.html", trading_results=client_config.orders, pnl_results=client_config.ticker_pnl)
 
     else:
         return render_template("error_auto_trading.html")
+
+@app.route('/sim_model_info')
+@login_required
+def sim_model_info():
+    return render_template("sim_model_info.html")
 
 
 # Market Data
