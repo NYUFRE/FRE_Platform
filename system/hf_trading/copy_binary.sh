33cec859f61ac039a5b0cc7f4a51d21c64ac8650
Robin Kong <mengxian68@gmail.com>
2022-02-12 11:25:58 -0500

    delete hf_trading folder

--- system/hf_trading/copy_binary.sh
+++
@@ -1,5 +0,0 @@
-#cp /Users/yirenwu/Desktop/nyu_class/Fall_2021/research/Capstone_Project/cmake-build-debug/Capstone_Project /Users/yirenwu/Desktop/nyu_class/Fall_2021/research/FRE_Platform/system/hf_trading/Capstone_Project
-cp -r /Users/yirenwu/Desktop/nyu_class/Fall_2021/research/Capstone_Project/cmake-build-debug/ /Users/yirenwu/Desktop/nyu_class/Fall_2021/research/FRE_Platform/system/hf_trading
-rm /Users/yirenwu/Desktop/nyu_class/Fall_2021/research/FRE_Platform/system/hf_trading/SPY_Market_Data.db
-rm /Users/yirenwu/Desktop/nyu_class/Fall_2021/research/FRE_Platform/system/hf_trading/example*
-
--- system/hf_trading/hf_trading.py
+++
@@ -1,6 +0,0 @@
-class HFTradingData:
-    def __init__(self):
-        self.data_list = []
-
-
-hf_trading_data = HFTradingData()
